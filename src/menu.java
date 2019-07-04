f6d16adf9984d40002f6c261264eeefbcbea0ea3
minhdien1710 <nguyenminhdien996@gmail.com>
2019-07-04 16:21:46 +0700

    added

---
+++ src/menu.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println('*');
+                    System.out.println("* *");
+                    System.out.println("* * *");
+                    System.out.println("* * * *");
+                    System.out.println("* * * * *");
+                    break;
+                case 2:
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("* * * * * * * *");
+                    System.out.println("* * * * * * * *");
+                    System.out.println("* * * * * * * *");
+                    System.out.println("* * * * * * * *");
+                    break;
+                default:
+                    System.out.println("ko hop le");
+            }
+        }while (choice!= 0);
+
+
+    }
+}
